d16990badd953fb7e3c5f162bb5a46a6eb5f0a4b
Spencer Haugh <spencer.haugh@gmail.com>
2021-01-04 16:08:42 -0500

    Twitter API rules added, get set and delete routes. Hardcoded askslowride hashtag as only rule.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Realtime Twitter API App
--- server/index.js
+++ server/index.js
@@ -3,4 +3,72 @@ const config = require('dotenv').config()
 const TOKEN = process.env.TWITTER_BEARER_TOKEN
 
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream/'
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id'
+
+const rules = [{ value: '#askslowride' }]
+
+// Get stream rules
+async function getRules() {
+    const response = await needle('get', rulesURL, {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+    // console.log(response.body)
+    return response.body
+}
+
+// Set stream rules
+async function setRules() {
+    const data = {
+        add: rules
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+    return response.body
+}
+
+// Delete stream rules
+async function deleteRules(rules) {
+    if(!Array.isArray(rules.data)) {
+        return null
+    }
+
+    const ids = rules.data.map((rule) => rule.id)
+
+    const data = {
+        delete: {
+            ids: ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+    return response.body
+}
+
+(async () => {
+    let currentRules
+    // Get all current stream rules
+    try {
+        currentRules = await getRules()
+
+        // Delete all current stream rules
+        await deleteRules(currentRules)
+
+        // Set (or reset) new rules per rules variable above
+        await setRules()
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+})()
