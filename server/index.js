27c6c8d3bf5100dd8ca32a106eb1323b5e332c89
Spencer Haugh <spencer.haugh@gmail.com>
2021-01-30 15:21:47 -0500

    rollback update

---
+++ server/index.js
@@ -0,0 +1,118 @@
+// Thank you to Brad Traversy for the inspiration to create this app!
+// https://www.youtube.com/watch?v=PjjjhGW4ceM
+
+const http = require('http')
+const path = require('path')
+const express = require('express')
+const socketIo = require('socket.io')
+const needle = require('needle');
+const config = require('dotenv').config()
+const TOKEN = process.env.TWITTER_BEARER_TOKEN
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+const server = http.createServer(app)
+const io = socketIo(server)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'index.html'))
+})
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id'
+
+const rules = [{ value: '#cxworlds' }]
+
+// Get stream rules
+async function getRules() {
+    const response = await needle('get', rulesURL, {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+    // console.log(response.body)
+    return response.body
+}
+
+// Set stream rules
+async function setRules() {
+    const data = {
+        add: rules
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+    return response.body
+}
+
+// Delete stream rules
+async function deleteRules(rules) {
+    if(!Array.isArray(rules.data)) {
+        return null
+    }
+
+    const ids = rules.data.map((rule) => rule.id)
+
+    const data = {
+        delete: {
+            ids: ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+    return response.body
+}
+
+function streamTweets(socket) {
+    const stream = needle.get(streamURL, {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+
+    stream.on('data', (data) => {
+        try {
+            const json = JSON.parse(data)
+            // console.log(json)
+            socket.emit('tweet', json)
+        } catch (error) {
+            
+        }
+    })
+}
+
+io.on('connection', async () => {
+    console.log('Client connected...')
+
+        let currentRules
+    // Get all current stream rules
+
+    try {
+        currentRules = await getRules()
+
+        // Delete all current stream rules
+        await deleteRules(currentRules)
+
+        // Set (or reset) new rules per rules variable above
+        await setRules()
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+
+    streamTweets(io)
+
+})
+
+
+server.listen(PORT, () => console.log(`Listening on Port ${PORT}`))
