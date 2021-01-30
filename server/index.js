2ef814c4e7f5022eec12c33237f266a57419c1f8
Spencer Haugh <spencer.haugh@gmail.com>
2021-01-30 13:31:36 -0500

    Changed focus to #cxworlds for the event

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
 const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id'
 
-const rules = [{ value: '#askslowride' }]
+const rules = [{ value: '#cxworlds' }]
 
 // Get stream rules
 async function getRules() {
